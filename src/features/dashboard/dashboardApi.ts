cb008ec7c2d1332ae47284565c272cd889bb0a58
Anantha Krishnan <anantha.krishnan@outlook.com>
2025-04-26 23:38:59 +0530

    setStart Date for Demo

--- src/features/dashboard/dashboardApi.ts
+++ src/features/dashboard/dashboardApi.ts
@@ -7,15 +7,25 @@ import {
   TrendingTagResponse,
 } from "../../types/models";
 
+const firstDayOfPreviousMonth = () => {
+  const date = new Date(Date.now());
+  date.setDate(1);
+  date.setMonth(date.getMonth() - 1);
+  return date.toISOString().split("T")[0];
+};
 export const ideaStatsApi = {
   fetchTopIdeas: () =>
     api.get<TopIdeasResponse>("/ideas/stats/top-ideas?limit=3"),
   fetchIdeaStatusDistribution: () =>
     api.get<StatusDistributionResponse>("/ideas/stats/status-distribution"),
   fetchIdeaSubmissionRate: () =>
-    api.get<IdeaSubmissionResponse>("/ideas/stats/idea-submissions"),
+    api.get<IdeaSubmissionResponse>(
+      `/ideas/stats/idea-submissions?startDate=${firstDayOfPreviousMonth()}`
+    ),
   fetchTopContributors: () =>
     api.get<TopContributorsResponse>("/ideas/stats/top-contributors"),
   fetchTrendingTags: () =>
-    api.get<TrendingTagResponse>("/ideas/stats/trending-tags"),
+    api.get<TrendingTagResponse>(
+      `/ideas/stats/trending-tags?startDate=${firstDayOfPreviousMonth()} `
+    ),
 };
